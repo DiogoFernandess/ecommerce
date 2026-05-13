3bf4f9b575406b6410cb71bfe96ceff43a63ba48
Diogo <diogosf2006@gmail.com>
2026-05-13 16:59:24 -0300

    feat: retirar item do carrinho e limpar carrinho

--- src/main/java/velaire/ecommmerce/business/service/CartService.java
+++ src/main/java/velaire/ecommmerce/business/service/CartService.java
@@ -1,9 +1,9 @@
 package velaire.ecommmerce.business.service;
 
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 import velaire.ecommmerce.infrastructure.entity.Cart;
 import velaire.ecommmerce.infrastructure.entity.CartItem;
@@ -47,6 +47,33 @@ public class CartService {
                             cart.addItem(newItem);
                         }
                 );
+        cartRepository.save(cart);
+    }
+
+    @Transactional
+    public void removeItem(String userId, Long productId){
+
+        Cart cart = cartRepository.findByUserId(userId)
+                .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "Carrinho vazio"));
+
+        boolean removed = cart.getItems().removeIf(item -> item.getProduct().getId().equals(productId));
+
+        if (!removed){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Produto não encontrado no carrinho");
+
+        }
+
+        cartRepository.save(cart);
+    }
+
+    @Transactional
+    public void clearCart(String userId){
+
+        Cart cart = cartRepository.findByUserId(userId)
+                .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "Carrinho vazio"));
+
+        cart.getItems().clear();
+
         cartRepository.save(cart);
     }
 }
