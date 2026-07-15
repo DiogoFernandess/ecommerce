7a1e08c3f48387a44186dc2e2269c265c90deb02
Diogo <diogosf2006@gmail.com>
2026-07-14 23:20:39 -0300

    UserController: Login e Registro de usuário

---
+++ src/main/java/velaire/ecommmerce/business/controller/UserController.java
@@ -0,0 +1,47 @@
+package velaire.ecommmerce.business.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import velaire.ecommmerce.infrastructure.security.JwtUtil;
+import velaire.ecommmerce.infrastructure.security.SecurityConfig;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+@SecurityRequirement(name = SecurityConfig.SECURITY_SCHEME)
+public class UserController {
+
+    private final UserService userService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
+
+
+    @PostMapping
+    public ResponseEntity<UserDto> saveUser(@RequestBody UserDto userDto){
+        return ResponseEntity.ok(userService.saveUser(userDto));
+    }
+
+    @PostMapping("/login")
+    public String login (@RequestBody UserDto userDto){
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(userDto.getEmail(),
+                        userDto.getPassword()
+                )
+        );
+        String role = authentication.getAuthorities().stream()
+                .findFirst() // Pega a primeira role que encontrar
+                .map(GrantedAuthority::getAuthority) // Converte para String
+                .orElse("ROLE_USER");
+
+        return "Bearer " + jwtUtil.generateToken(authentication.getName(), role);
+    }
+
+}
